6393d4b8c193dd3b6c0df5f53edd9aff8b7e0f01
umasenthil <uma_senthil@hotmail.com>
2016-01-08 04:10:37 +0000

    Added hash rocket for foreign keys

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ActiveRecord::Base
   has_many :game_pieces
-  belongs_to :player_white, :class_name => "User", :foreign_key :player_white_id
-  belongs_to :player_black, :class_name => "User", :foreign_key :player_black_id
+  belongs_to :player_white, :class_name => "User", :foreign_key => "player_white_id"
+  belongs_to :player_black, :class_name => "User", :foreign_key => "player_black_id"
 end
