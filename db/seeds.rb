06e936113c3308739be6da79f1c0d38131fc1195
gruzzlymug <kevin@gruzzlymug.com>
2016-01-08 03:08:52 +0000

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Create these test users with this command:
+# rake db:seed
+puts 'Creating test users'
+test_users = [ 'test@test.com', 'test2@test.com' ]
+test_users.each do |email|
+    User.create!(email: email, password: 'testtest', password_confirmation: 'testtest') unless User.find_by_email(email).present?
+end
