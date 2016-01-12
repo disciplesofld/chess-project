68a629248d3ef7c8e66fc9c4fb347138b2d94127
sellis7 <sellis7@users.noreply.github.com>
2016-01-12 12:51:14 -0500

    README header update/Established 1st player saving game to play/Navigation tweaks

--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -4,4 +4,27 @@ class GamesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "redirects to games index" do
+    user = FactoryGirl.create(:user)
+    sign_in user
+    assert_response :success
+  end
+
+  test "current games can be viewed?" do
+    assert_difference 'Game.count' do
+      current_game = FactoryGirl.create(:game)
+    end
+    assert_equal 1, Game.count
+  end
+
+  test "second user can join game" do
+    user_id = 2
+    started_game = FactoryGirl.create(:game)
+    assert_no_difference 'Game.count' do
+      put :update, id: started_game.id, game: {player_white_id: user_id}
+    end
+    # assert_redirected_to place_path(place)
+  end
+
 end
--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -4,4 +4,23 @@ class GameTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "user can start game" do
+    started_game = FactoryGirl.create(:game)
+    assert started_game.save  # persisted?
+  end
+
+  test "second user can view game to join" do
+    started_game = FactoryGirl.create(:game)
+    Game.where(player_white_id: 0).find_each do |game|
+      return joinable_game = game
+    end
+    assert_not_nil joinable_game
+  end
+
+  # test "second user can join game" do
+  #   started_game = Game.create(id: 1, player_black_id: 1, name_for_game: "royalty bout")
+  #
+  # end
+
 end
