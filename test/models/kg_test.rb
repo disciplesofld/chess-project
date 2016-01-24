666b600e43d4e8390c929ff27a94165e0bf1f451
gruzzlymug <kevin@gruzzlymug.com>
2016-01-24 19:22:02 +0000

    change factories

---
+++ test/models/kg_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+module KG
+  class UserTest < ActiveSupport::TestCase
+    test "the factory can create a user" do
+      u = FactoryGirl.create(:user)
+      p u
+      assert u
+    end
+  end
+
+  class GameTest < ActiveSupport::TestCase
+    test "the factory can create a user" do
+      g = FactoryGirl.create(:game)
+      p g
+      assert g
+
+      w = FactoryGirl.create(:user, email: 'wunderbar@underground.org')
+      p w
+      h = FactoryGirl.create(:game, player_white: w)
+      p h
+      assert h
+    end
+  end
+end
