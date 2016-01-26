b12cca7e5aa3f1142c53f529e89387b3cc0efc36
disciplesofld <lby75429@gmail.com>
2016-01-26 17:49:00 +0000

    Capture Method still need STI

--- app/models/game_piece.rb
+++ app/models/game_piece.rb
@@ -3,7 +3,24 @@ class GamePiece < ActiveRecord::Base
   belongs_to :user
   
   def move_piece(new_x, new_y)
-    self.update_attributes(:x => new_x, :y => new_y)
+    #define the piece on new position
+    friend_or_enemy = self.game.game_pieces.where(:x => new_x, :y => new_y).first
+
+    #if new posistion is an enemy, capture it
+    if friend_or_enemy.present? && self.user_id != friend_or_enemy.user_id
+      friend_or_enemy.update_attributes(:x => nil, :y => nil, :status => 0)
+      if is_move_validated?(new_x, new_y)
+        self.update_attributes(:x => new_x, :y => new_y)
+      end
+    #if it's a friend piece, return false
+    elsif friend_or_enemy.present? && self.user_id == friend_or_enemy.user_id
+      return false
+    else
+      if is_move_validated?(new_x, new_y)
+        self.update_attributes(:x => new_x, :y => new_y)
+      end
+    end
+    return true
   end
   
   # While scanning for opponent, skip the actual piece. 
--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,23 +1,14 @@
 class Knight < GamePiece
 
-	def move_piece(new_x, new_y)
-        current_x = self.x
-        current_y = self.y
-        next_x = new_x.to_i
-        next_y = new_y.to_i
-        #if match the rule of knight, move to the new position
-        if is_move_validated?(current_x, current_y, next_x, next_y)
-            self.update_attributes(:x => new_x, :y => new_y)
-        end
-    end
+
 
         
-    def is_move_validated?(current_x, current_y, next_x, next_y)
+    def is_move_validated?(new_x, new_y)
         #return false if piece doen't move
-        return false if current_x == next_x && current_y == next_y
+        return false if self.x == new_x&& self.y == new_y
 
         # define the rule of knight
-        if (current_x - next_x).abs + (current_y - next_y).abs == 3
+        if (self.x.to_i - new_x.to_i).abs + (self.y.to_i - new_y.to_i).abs == 3
             return true
         else
             return false
