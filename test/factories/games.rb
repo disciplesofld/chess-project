7a39b083f3ce0b0b91d596486caeed094719ce51
sellis7 <sellis7@users.noreply.github.com>
2016-01-27 11:35:56 -0500

    Catching HEAD up to repository master

--- test/factories/games.rb
+++ test/factories/games.rb
@@ -1,18 +1,11 @@
 FactoryGirl.define do
-
   sequence :name_for_game do |n|
     "game#{n}"
   end
 
-  sequence :id do |n|
-    n
-  end
-
   factory :game do
-    id
-    player_black_id 1 # 1st user
-    player_white_id 0 # 1st user
+    association :player_black, factory: :user
+    association :player_white, factory: :user
     name_for_game
   end
-
 end
---
+++ test/models/kg_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+# rake test test/models/kg_test.rb
+module KG
+  class UserTest < ActiveSupport::TestCase
+    test "the factory can create a user" do
+      u = FactoryGirl.create(:user)
+      p u
+      assert u
+    end
+  end
+
+  class GameTest < ActiveSupport::TestCase
+    test "the factory can create a game" do
+      g = FactoryGirl.create(:game)
+      p g
+      assert g
+
+      w = FactoryGirl.create(:user, email: 'wunderbar@underground.org')
+      p w
+      h = FactoryGirl.create(:game, player_white: w)
+      p h
+      assert h
+    end
+  end
+end
