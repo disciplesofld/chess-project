55d22f6c9c9183c04bfb216e3e3ab41568ee9223
Hiromi Kawanishi <contacthiromi@gmail.com>
2016-01-29 18:18:34 +0000

    add bishop move validation and remove move_piece method from queen.rb

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,6 +1,6 @@
 class Knight < GamePiece
   # TODO remove. no need for this here
-	# def move_piece(new_x, new_y)
+  # def move_piece(new_x, new_y)
   #       current_x = self.x
   #       current_y = self.y
   #       next_x = new_x.to_i
@@ -11,22 +11,22 @@ class Knight < GamePiece
   #       end
   #   end
 
-    def valid_move?(next_x, next_y)
-      # these values should be converted to integers before being passed in, if it is necessary
-			n_x = next_x.to_i
-	    n_y = next_y.to_i
+  def valid_move?(next_x, next_y)
+    # these values should be converted to integers before being passed in, if it is necessary
+    n_x = next_x.to_i
+    n_y = next_y.to_i
 
-      #return false if piece doen't move
-			# Not needed anymore; current location not available as a move (Sherilyn)
-      # return false if self.x == n_x && self.y == n_y
+    #return false if piece doen't move
+    # Not needed anymore; current location not available as a move (Sherilyn)
+    # return false if self.x == n_x && self.y == n_y
 
-      # define the rule of knight
-      if (self.x - n_x).abs + (self.y - n_y).abs == 3
-				self.move_piece(next_x, next_y)
-        return true
-      else
-        return false
-      end
+    # define the rule of knight
+    if (self.x - n_x).abs + (self.y - n_y).abs == 3
+      self.move_piece(next_x, next_y)
+      return true
+    else
+      return false
     end
+  end
 
 end
