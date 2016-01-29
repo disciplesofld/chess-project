91ee8424ed15749280871e789b350d8284c91b69
sellis7 <sellis7@users.noreply.github.com>
2016-01-29 11:48:07 -0500

    Get player pieces on board

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -3,13 +3,11 @@ class Rook < GamePiece
 	def valid_move?(new_x, new_y)
 		# cannot land on similar player piece - handled by is_obstructed? method, game.rb
 		# any pieces in path from start to finish? - handled by is_obstructed? method, game.rb
-		move_x = new_x.to_i
-		move_y = new_y.to_i
 
 		# self.x = Column index; self.y = Row index (reversed: bottom-to-top)
 		# Moves only (vertically) || only (horizontally)
-		if ((self.x == move_x) && (self.y > move_y || self.y < move_y)) || ((self.y == move_y) && (self.x > move_x || self.x < move_x))
-			self.move_piece(new_x, new_y)
+		if ((self.x == new_x) && (self.y > new_y || self.y < new_y)) || ((self.y == new_y) && (self.x > new_x || self.x < new_x))
+			# self.move_to(new_x, new_y) - now obtaining function from game_pieces.rb
 			return true
 		end
 	end
