66dabe1ee2d21f2f585de3fa9c6422b908ee7deb
sellis7 <sellis7@users.noreply.github.com>
2016-02-01 17:06:24 -0500

    Pawn validation mostly complete - need db moved status

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,7 +1,26 @@
 class Pawn < GamePiece
   def valid_move?(new_x, new_y)
-    # TODO: Implement!
-    true # ONLY UNTIL I GET TO VALIDATE
+    # determine if pawn is moving ahead or diagonal to take opponent piece - use capture method
+    # determine if 1st or 2nd move
+    # Based on move, 1 or 2 blocks only forward
+    # self.x = Column index; self.y = Row index (reversed: bottom-to-top)
+
+    if self.x == new_x
+
+      if self.user_id == game.player_white_id # White pawn
+        if (moved == true && new_y == (self.y + 1)) || \
+          (moved == false && new_y <= (self.y + 2))
+          return true
+        end
+      else # Black pawn
+        if (moved == true && new_y == (self.y - 1)) || \
+          (moved == false && new_y <= (self.y - 2))
+          return true
+        end
+      end
+
+    end
+
   end
 
   def icon
